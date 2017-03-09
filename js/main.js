cc13ae77dccd9de687b22f94e4f98abab47c3b42
Tien Thanh <tienthanh100696@gmail.com>
2017-03-09 20:27:22 +0700

    move the ship config 2: add the effect

--- js/main.js
+++ js/main.js
@@ -39,52 +39,125 @@ var create = function(){
 
 // update game state each frame
 var update = function(){
+
   if(Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
-    Nakama.player.position.y -= 10;
-
-    if(Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
-      Nakama.player.position.x -= 10;
-    }
-
-    if(Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-      Nakama.player.position.x += 10;
-    }
-  }
-  else if(Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
-    Nakama.player.position.y += 10;
-
-    if(Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
-      Nakama.player.position.x -= 10;
-    }
-
-    if(Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-      Nakama.player.position.x += 10;
-    }
-  }
-  else if(Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
-    Nakama.player.position.x -= 10;
-
-    if(Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
+    if(Nakama.player.position.y > 10){
       Nakama.player.position.y -= 10;
+
+      if(Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
+        if(Nakama.player.position.x > 10){
+          Nakama.player.position.x -= 10;
+
+        } else { //if(Nakama.player.position.x < 10)
+          Nakama.player.position.x += 1;
+
+        }
+
+      } else if(Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        if(Nakama.player.position.x < 550){
+          Nakama.player.position.x += 10;
+
+        } else { //if(Nakama.player.position.x > 550)
+          Nakama.player.position.x -= 1;
+
+        }
+
+      }
+
+    } else{ //if(Nakama.player.position.y > 10)
+      Nakama.player.position.y += 1;
+
     }
 
-    if(Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
+  } else if(Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
+    if(Nakama.player.position.y < 880){
       Nakama.player.position.y += 10;
+
+      if(Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
+        if(Nakama.player.position.x > 10){
+          Nakama.player.position.x -= 10;
+
+        } else {//if(Nakama.player.position.x < 10)
+          Nakama.player.position.x += 1;
+
+        }
+
+      }
+      else if(Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        if(Nakama.player.position.x < 550){
+          Nakama.player.position.x += 10;
+
+        } else {//if(Nakama.player.position.x < 550)
+          Nakama.player.position.x -= 1;
+
+        }
+
+      }
+
+    } else {//if(Nakama.player.position.y > 880)
+      Nakama.player.position.y -= 1;
+
     }
+
+  } else if(Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
+    if(Nakama.player.position.x > 10){
+      Nakama.player.position.x -= 10;
+
+      if(Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
+        if(Nakama.player.position.y > 10){
+          Nakama.player.position.y -= 10;
+
+        } else {//if(Nakama.player.position.y < 10)
+          Nakama.player.position.y += 1;
+
+        }
+
+      } else if(Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
+        if(Nakama.player.position.y < 880){
+          Nakama.player.position.y += 10;
+
+        } else {//if(Nakama.player.position.y > 880)
+          Nakama.player.position.y -= 1;
+
+        }
+
+      }
+
+    } else {//if(Nakama.player.position.x < 10)
+      Nakama.player.position.x += 1;
+
+    }
+
+  } else if(Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+    if(Nakama.player.position.x < 550){
+      Nakama.player.position.x += 10;
+
+      if(Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
+        if(Nakama.player.position.y > 10){
+          Nakama.player.position.y -= 10;
+
+        } else {//if(Nakama.player.position.y < 10)
+          Nakama.player.position.y += 1;
+
+        }
+
+      } else if(Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
+        if(Nakama.player.position.y < 880){
+          Nakama.player.position.y += 10;
+
+        } else {//if(Nakama.player.position.y > 880)
+          Nakama.player.position.y -= 1;
+
+        }
+
+      }
+
+    } else {//if(Nakama.player.position.x > 550)
+      Nakama.player.position.x -= 1;
+
+    }
+
   }
-  else if(Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-    Nakama.player.position.x += 10;
-
-    if(Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
-      Nakama.player.position.y -= 10;
-    }
-
-    if(Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
-      Nakama.player.position.y += 10;
-    }
-  }
-
-  
 
 }
 
