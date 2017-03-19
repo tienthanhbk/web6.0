eb8a6b0f5090618f30a8ca45d30ae8be83821c21
Tien Thanh <tienthanh100696@gmail.com>
2017-03-19 19:44:28 +0700

    (code a Duc) Turn slowly if the angle is too large

--- js/BulletType2Controller.js
+++ js/BulletType2Controller.js
@@ -17,10 +17,49 @@ class BulletType2Controller extends BulletController{
     // 2. If no target is available keep on going straight
     if(!this.target) return;
 
-    // 3. Change direction toward target
+    // 3. Get direction toward target
     var direction = Phaser.Point.subtract(this.target.position, this.sprite.position);
-    this.sprite.body.velocity = direction.setMagnitude(BulletType2Controller.BULLET_SPEED);
-    this.sprite.angle = Math.atan2(direction.x, -direction.y) * (180/Math.PI);
+
+    // 4. Turn slowly if the angle is too large
+    var currentAngle = Nakama.game.math.radToDeg(
+      Nakama.game.math.angleBetween(
+        0,
+        0,
+        this.sprite.body.velocity.x,
+        this.sprite.body.velocity.y
+      )
+    );
+
+    var directionAngle = Nakama.game.math.radToDeg(
+      Nakama.game.math.angleBetween(
+        0,
+        0,
+        direction.x,
+        direction.y
+      )
+    );
+
+    var deltaAngle = directionAngle - currentAngle;
+
+    // Ensure deltaAngle is between -180 & 180
+    if(deltaAngle > 180) deltaAngle -= 360;
+    if(deltaAngle < -180) deltaAngle += 360;
+
+    // Ensure deltaAngle does not exceed max turn rate
+    var maxDelta = BulletType2Controller.TURN_RATE * Nakama.game.time.physicsElapsed;
+    if(deltaAngle > maxDelta) deltaAngle = maxDelta;
+    if(deltaAngle < -maxDelta) deltaAngle = -maxDelta;
+
+    // Apply new direction based on new angle
+    var newAngle = currentAngle + deltaAngle;
+    var newDirection = new Phaser.Point(
+      Math.cos(Nakama.game.math.degToRad(newAngle)),
+      Math.sin(Nakama.game.math.degToRad(newAngle))
+    );
+
+    // 5. Set new velocity
+    this.sprite.body.velocity = newDirection.setMagnitude(BulletType2Controller.BULLET_SPEED);
+    this.sprite.angle = Math.atan2(newDirection.x, -newDirection.y) * (180/Math.PI);
   }
 
   getNewTarget(){
@@ -28,6 +67,7 @@ class BulletType2Controller extends BulletController{
   }
 }
 
+BulletType2Controller.TURN_RATE = 200;
 BulletType2Controller.BULLET_SPEED = 900;
 BulletType2Controller.SPRITE_NAME = "BulletType2.png";
 BulletType2Controller.DAMAGE = 3;
